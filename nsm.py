5d989aa87746d2322534ee40d4fce9aa0d9fe440
BuildTools <unconfigured@null.spigotmc.org>
2020-04-28 20:06:20 +1000

    modified:   nsm.py

--- nsm.py
+++ nsm.py
@@ -1,53 +1,62 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F 
+import torch.nn.functional as F
+from random import randint
+from itertools import permutations, repeat
+
+
+def to_rand(token, emd_dim):
+    return torch.rand(emd_dim)
+
 
 class NSM(nn.Module):
-
     embd_dim = 7
     out_dim = 4
     batch = 10
     N = 3
-    L = 3 
+    L = 3
 
     def __init__(self):
         super().__init__()
 
         self.W = torch.eye(
-            self.embd_dim, 
+            self.embd_dim,
             requires_grad=True
         )
 
         self.property_W = torch.stack(
-            [torch.eye(self.embd_dim, requires_grad=True) for _ in range(self.L + 1)], 
+            repeat(
+                torch.eye(self.embd_dim, requires_grad=True), 
+                range(self.L + 1)
+            ),
             dim=0
         )
 
         self.W_L_plus_1 = torch.eye(
-            self.embd_dim, 
+            self.embd_dim,
             requires_grad=True
         )
 
         self.W_r = nn.Linear(
-            self.embd_dim, 
-            1, 
+            self.embd_dim,
+            1,
             bias=False
         )
 
         self.W_s = nn.Linear(
-            self.embd_dim, 
-            1, 
+            self.embd_dim,
+            1,
             bias=False
         )
 
         # encoder 
         self.encoder_lstm = nn.LSTM(
-            input_size=self.embd_dim, 
-            hidden_size=self.embd_dim, 
-            batch_first=True, 
+            input_size=self.embd_dim,
+            hidden_size=self.embd_dim,
+            batch_first=True,
             bidirectional=False
         )
-       
+
         # recurrent decoder
         self.decoder_lstm = nn.LSTM(
             input_size=self.embd_dim, 
@@ -59,9 +68,21 @@ class NSM(nn.Module):
         # final classifier
         self.classifier = nn.Sequential(
             nn.Linear(2*self.embd_dim, 2*self.embd_dim), 
-            nn.ELU(), 
+            nn.ELU(),
             nn.Linear(2*self.embd_dim, self.out_dim)
         )
 
-    def forward(self, questions, C, D, E, S, adjacency_mask):
+    def forward(self, questions, C, D, E, S, adjacency_mask):     
+        embd_questions = torch.stack( 
+            map(lambda x: torch.stack(to_rand(x, self.embd_dim)), questions)
+        )
+
+        P_i = torch.softmax(torch.bmm(
+            torch.bmm(
+                embd_questions,
+                self.W.expand(self.batch, self.embd_dim, self.embd_dim)
+            ),
+            C.expand(self.batch, -1, self.embd_dim).transpose(1, 2)
+        ), dim=2)
+
         return 0
